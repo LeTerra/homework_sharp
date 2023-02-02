c4f1de7b628dc094efd03ae19166b596190de20c
LeTerra <t.v.sitdikov@gmail.com>
2023-02-02 16:47:47 +0100

    Task Finished.

---
+++ Task002_HomeMinMax/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Please input the first number.");
+string StringUserNumA = Console.ReadLine();
+int UserNumA = Convert.ToInt32(StringUserNumA);
+Console.WriteLine("Please input the second number.");
+string StringUserNumB = Console.ReadLine();
+int UserNumB = Convert.ToInt32(StringUserNumB);
+int max = 0;
+int min = 0;
+
+if(UserNumA == UserNumB) Console.WriteLine("The two numbers are equal to each other.");
+else
+{
+if(UserNumA > UserNumB) 
+{max = UserNumA; 
+min = UserNumB;}
+else
+{
+  {max = UserNumB; 
+min = UserNumA;}  
+}
+
+{
+    Console.Write("max = ");
+    Console.Write(max);
+    Console.Write("; ");
+    Console.Write("min = ");
+    Console.Write(min);
+}}
