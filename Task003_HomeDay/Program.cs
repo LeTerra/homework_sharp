958186059dbc4ee4bc4454c85fa9ebfb4d5f9c69
LeTerra <t.v.sitdikov@gmail.com>
2023-02-02 16:31:33 +0100

    Renamed to a correct number.

---
+++ Task003_HomeDay/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please input the week-day's number.");
+string userInput = Console.ReadLine();
+
+{
+if (userInput == "1") userInput = "Monday";
+if (userInput == "2") userInput = "Tuesday";
+if (userInput == "3") userInput = "Wednesday";
+if (userInput == "4") userInput = "Thursday";
+if (userInput == "5") userInput = "Friday";
+if (userInput == "6") userInput = "Saturday";
+if (userInput == "7") userInput = "Sunday";
+else
+{
+    Console.WriteLine("There is no day that would correspons to that number.");
+}
+Console.Write("Your weekday is ");
+Console.Write(userInput);
+}
