62b99e2fb5aaaba66725b1ce8b50d6d2916a3e58
LeTerra <t.v.sitdikov@gmail.com>
2023-02-02 16:56:42 +0100

    Task Finished.

---
+++ Task004_HomeMax/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Please input the first number.");
+string StringUserNumA = Console.ReadLine();
+int UserNumA = Convert.ToInt32(StringUserNumA);
+Console.WriteLine("Please input the second number.");
+string StringUserNumB = Console.ReadLine();
+int UserNumB = Convert.ToInt32(StringUserNumB);
+Console.WriteLine("Please input the third number.");
+string StringUserNumC = Console.ReadLine();
+int UserNumC = Convert.ToInt32(StringUserNumC);
+int max = UserNumA;
+
+if(UserNumA > max) max = UserNumA;
+if(UserNumB > max) max = UserNumB;
+if(UserNumC > max) max = UserNumC;
+
+Console.Write("The max number out of the three is ");
+Console.Write(max);
