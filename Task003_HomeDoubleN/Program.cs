96c11aa373ff0846950cfa4a8d40e334ab12f7f2
LeTerra <t.v.sitdikov@gmail.com>
2023-02-02 15:23:44 +0100

    Task Finished

---
+++ Task003_HomeDoubleN/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Please input your number:");
+string strUserInput = Console.ReadLine();
+int UserInput = Convert.ToInt32(strUserInput);
+int ogUserInput = UserInput;
+int count = 0;
+
+while(count < (ogUserInput * 2)+1)
+{
+Console.Write(-UserInput);
+Console.Write(", ");
+UserInput = UserInput - 1;
+count = count + 1;
+}
+
