13b92552198b88ab7052acb51f510edd06db2630
LeTerra <t.v.sitdikov@gmail.com>
2023-02-02 14:44:40 +0100

    Task 001 - Finished

---
+++ Task001_HomeSquare/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Please input the first number.");
+string StringUserNumA = Console.ReadLine();
+int UserNumA = Convert.ToInt32(StringUserNumA);
+Console.WriteLine("Please input the second number.");
+string StringUserNumB = Console.ReadLine();
+int UserNumB = Convert.ToInt32(StringUserNumB);
+
+int result = UserNumA / UserNumB;
+
+if(result == UserNumB) Console.WriteLine("The first number is the square of the second.");
+else
+{
+    Console.WriteLine("The first number is NOT the square of the second.");
+}
