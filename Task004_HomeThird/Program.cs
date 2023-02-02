5b57cc0a367bf9d7845011ff96e00679da405a48
LeTerra <t.v.sitdikov@gmail.com>
2023-02-02 16:27:46 +0100

    Task Finished

---
+++ Task004_HomeThird/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Please input your three-digit number:");
+string strUserInput = Console.ReadLine();
+int UserInput = Convert.ToInt32(strUserInput);
+if(UserInput < 100)
+{
+    Console.WriteLine("This is NOT a three-digit number.");
+}
+else{
+if(UserInput >= 1000)
+{
+    Console.WriteLine("This is NOT a three-digit number.");
+}
+else{
+int ogUserInput = UserInput;
+UserInput = UserInput/10;
+int InputTakeAway = ogUserInput - (UserInput*10);
+Console.Write("The first digit of the given number is ");
+Console.Write(InputTakeAway);
+}
+}
