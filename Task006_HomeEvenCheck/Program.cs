1ea38ff0dd3fe53feaa4a7d6c7d49e79563b6191
LeTerra <t.v.sitdikov@gmail.com>
2023-02-02 17:42:09 +0100

    Task Finished.

---
+++ Task006_HomeEvenCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Please input your number:");
+string strUserInput = Console.ReadLine();
+double UserInput = Convert.ToDouble (strUserInput);
+if(UserInput == 1) Console.WriteLine("The number is odd.");
+else
+{
+while(UserInput > 1)
+{
+    UserInput = UserInput / 2;
+}
+if(UserInput == 1) 
+{
+    Console.WriteLine("The number is even.");
+}
+else
+{
+    Console.WriteLine("The number is odd.");
+}
+}
