1987881026904d17c505562d3f7346b069a1b213
LeTerra <t.v.sitdikov@gmail.com>
2023-02-03 11:49:38 +0100

    Task015 - Finished.

---
+++ Task015_HomeDayCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Please write the number, so we could identify if it belongs to a weekday.");
+string strUserInput = Console.ReadLine();
+int UserInput = Convert.ToInt32(strUserInput);
+
+if(UserInput < 1 || UserInput > 7)
+{
+    Console.WriteLine("There is no weekday asssigned to the inputted number.");
+}
+else
+{
+    if(UserInput == 6 || UserInput == 7)
+    {Console.WriteLine("This number is assigned to a weekend.");}
+    else{
+        Console.WriteLine("This number is NOT assigned to a weekend.");
+    }
+
+}
