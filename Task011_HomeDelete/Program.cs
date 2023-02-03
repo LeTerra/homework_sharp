09334cfcf491018d404c4507015333cf91ab21a8
LeTerra <t.v.sitdikov@gmail.com>
2023-02-03 10:16:56 +0100

    Task011 - Finished.

---
+++ Task011_HomeDelete/Program.cs
@@ -0,0 +1,8 @@
+﻿int RndInput = new Random().Next(100,999);
+Console.WriteLine($"The selected number is {RndInput}.");
+
+int defIntgr01 = (RndInput % 10);
+int defIntgr03 = (RndInput / 100)*10;
+int OutputIntgr = defIntgr01 + defIntgr03;
+
+Console.WriteLine($"The output number is {OutputIntgr}.");
