e23cc8b416b3a556758085ac9b1a7fd2ec0cee04
LeTerra <t.v.sitdikov@gmail.com>
2023-02-03 11:42:01 +0100

    Task014 - Finished.

---
+++ Task014_HomeDoubleCheck/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Please input your number:");
+string strNumber01 = Console.ReadLine();
+double Number01 = Convert.ToDouble(strNumber01);
+
+if(Number01 % 23 == 0 && Number01 % 7 == 0)
+{
+Console.WriteLine($"The number is divisible by both 7 and 23.");
+}
+else
+{
+    Console.WriteLine($"The number is NOT divisible by both 7 and 23.");
+}
+
