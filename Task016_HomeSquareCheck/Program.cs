30344140e1bcb491f72cee36c54068b2e72f10bc
LeTerra <t.v.sitdikov@gmail.com>
2023-02-03 11:54:31 +0100

    Task016 - Finished.

---
+++ Task016_HomeSquareCheck/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Please input your first number:");
+string strNumber01 = Console.ReadLine();
+double Number01 = Convert.ToDouble(strNumber01);
+Console.WriteLine("Please input your second number:");
+string strNumber02 = Console.ReadLine();
+double Number02 = Convert.ToDouble(strNumber02);
+
+if(Number01*Number01 == Number02 || Number02*Number02 == Number01)
+{
+    Console.WriteLine("One of the numbers IS the square of the other.");
+}
+else
+{
+Console.WriteLine("None of the numbers are the square of the other.");
+}
