f039bcd17de4df16bf1d3d2dbbae61c418a251ef
LeTerra <t.v.sitdikov@gmail.com>
2023-02-03 10:35:20 +0100

    Task012 - Finished.

---
+++ Task012_HomeMutliplicity/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Please input your first number:");
+string strNumber01 = Console.ReadLine();
+double Number01 = Convert.ToDouble(strNumber01);
+Console.WriteLine("Please input your second number:");
+string strNumber02 = Console.ReadLine();
+double Number02 = Convert.ToDouble(strNumber02);
+
+if(Number01 % Number02 == 0) Console.WriteLine("The first number IS the multiplication of the second number.");
+else{
+    Console.WriteLine("The first number IS NOT the multiplication of the second number.");
+}
