c1a9af8d58be459fca9010fb104484bbc6c143ad
LeTerra <t.v.sitdikov@gmail.com>
2023-02-03 10:03:20 +0100

    Task010 - Finished.

---
+++ Task010_HomeSecond/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please input your three-digit number:");
+string strUserInput = Console.ReadLine();
+int UserInput = Convert.ToInt32(strUserInput);
+
+if(UserInput < 100)
+{Console.WriteLine("This is NOT a three-digit number.");}
+else{
+if(UserInput >= 1000)
+{Console.WriteLine("This is NOT a three-digit number.");}
+else{
+
+int defIntgr01 = UserInput % 1;
+int defIntgr03 = (UserInput / 100)*100;
+int defIntgr02 = (UserInput - (defIntgr01 + defIntgr03))/10;
+
+Console.WriteLine($"The second digit is {defIntgr02}.");
+
+}}
