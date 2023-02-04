cf5571b07e01c5bed3a8c4703b776bda1dd557d4
LeTerra <t.v.sitdikov@gmail.com>
2023-02-04 09:39:43 +0100

    Task018 - Finished.

---
+++ Task018_HomeReverse/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Please input the number of your coordinate plane:");
+string strKeyNumber = Console.ReadLine();
+int KeyNumber = Convert.ToInt32(strKeyNumber);
+
+if(KeyNumber < 1 || KeyNumber > 4) Console.WriteLine("Such a plane does not exist.");
+else
+{
+    if(KeyNumber == 1) Console.WriteLine("This plane includes numbers ranging from x > 0 and y > 0.");
+    if(KeyNumber == 2) Console.WriteLine("This plane includes numbers ranging from x < 0 and y > 0.");
+    if(KeyNumber == 3) Console.WriteLine("This plane includes numbers ranging from x < 0 and y < 0.");
+    if(KeyNumber == 4) Console.WriteLine("This plane includes numbers ranging from x > 0 and y < 0.");
+    else
+    {
+      Console.WriteLine("Such a plane does not exist.");  
+    }
+}
