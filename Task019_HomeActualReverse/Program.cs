a0e4c9226734fb2e4be91e0691d6c6328d5014b2
LeTerra <t.v.sitdikov@gmail.com>
2023-02-04 11:47:36 +0100

    Task 019 - Finished.

---
+++ Task019_HomeActualReverse/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please input your 5-digit number.");
+string strUserInput = Console.ReadLine();
+int UserInput = Convert.ToInt32(strUserInput);
+
+if(UserInput >= 100000 || UserInput < 10000) Console.WriteLine("This is NOT a 5-digit number.");
+else
+{
+    int idDigit01 = UserInput/10000;
+    int idDigit02 = (UserInput/1000) - (idDigit01*10);
+    int idDigit05 = UserInput%10;
+    int idDigit04 = ((UserInput%100) - idDigit05)/10;
+
+    if(idDigit01 == idDigit05 && idDigit02 == idDigit04) Console.WriteLine("The number IS a palidrome.");
+    else
+    {
+        Console.WriteLine("The number IS NOT a palidrome.");
+    }
+}
