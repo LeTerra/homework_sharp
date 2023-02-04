de2ffb7d9f11a3cce6b182814ce821071ac837da
LeTerra <t.v.sitdikov@gmail.com>
2023-02-04 09:17:24 +0100

    Task017 - Finished.

---
+++ Task017_HomeCoordinates/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Please input your X coordinate:");
+string strXCoordinate = Console.ReadLine();
+int XCoordinate = Convert.ToInt32(strXCoordinate);
+Console.WriteLine("Please input your Y coordinate:");
+string strYCoordinate = Console.ReadLine();
+int YCoordinate = Convert.ToInt32(strYCoordinate);
+
+if(XCoordinate == 0 || YCoordinate == 0) Console.WriteLine("Since one of the coordinates is located at 0, it belongs to neither plane.");
+else
+{
+    if(XCoordinate > 0 && YCoordinate > 0) Console.WriteLine("The coordinates inputted blong to the first plane.");
+    if(XCoordinate < 0 && YCoordinate > 0) Console.WriteLine("The coordinates inputted blong to the second plane.");
+    if(XCoordinate < 0 && YCoordinate < 0) Console.WriteLine("The coordinates inputted blong to the third plane.");
+    if(XCoordinate > 0 && YCoordinate < 0) Console.WriteLine("The coordinates inputted blong to the fourth plane.");
+}
+
