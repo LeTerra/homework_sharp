e1df6e16eba7a28bfe573dc588e7425bdbf395a3
LeTerra <t.v.sitdikov@gmail.com>
2023-02-04 14:30:50 +0100

    Task022 - Finished.

---
+++ Task022_HomeSquareList/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Please input your number:");
+string strUserInput = Console.ReadLine();
+double UserInput = Convert.ToDouble(strUserInput);
+
+
+if(UserInput<1) Console.WriteLine("There are no integer squares up until this number.");
+else
+{
+Console.Write("Here is the list of all the squares up until (and incl.) the given number: ");
+for(double i = 1; i < UserInput+1; i++)
+{
+    double iSquare = Math.Pow(i,2);
+    Console.Write($"{iSquare} ");
+}
+}
