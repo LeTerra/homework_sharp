08e86d441084da595979acd2a51ec5ea39812f0d
LeTerra <t.v.sitdikov@gmail.com>
2023-03-06 10:28:40 +0100

    Fixed Null Value Error.

--- Task004_HomeMax/Program.cs
+++ Task004_HomeMax/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Please input the first number.");
-string StringUserNumA = Console.ReadLine();
+string StringUserNumA = Console.ReadLine()??string.Empty;
 int UserNumA = Convert.ToInt32(StringUserNumA);
 Console.WriteLine("Please input the second number.");
-string StringUserNumB = Console.ReadLine();
+string StringUserNumB = Console.ReadLine()??string.Empty;
 int UserNumB = Convert.ToInt32(StringUserNumB);
 Console.WriteLine("Please input the third number.");
-string StringUserNumC = Console.ReadLine();
+string StringUserNumC = Console.ReadLine()??string.Empty;
 int UserNumC = Convert.ToInt32(StringUserNumC);
 int max = UserNumA;
 
