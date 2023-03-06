5496e382ab215011585f605db7ff0f1eca9a6f40
LeTerra <t.v.sitdikov@gmail.com>
2023-03-06 10:30:00 +0100

    Fixed Null Value Error.

--- Task012_HomeMutliplicity/Program.cs
+++ Task012_HomeMutliplicity/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Please input your first number:");
-string strNumber01 = Console.ReadLine();
+string strNumber01 = Console.ReadLine()??string.Empty;
 double Number01 = Convert.ToDouble(strNumber01);
 Console.WriteLine("Please input your second number:");
-string strNumber02 = Console.ReadLine();
+string strNumber02 = Console.ReadLine()??string.Empty;
 double Number02 = Convert.ToDouble(strNumber02);
 
 if(Number01 % Number02 == 0) Console.WriteLine("The first number IS the multiplication of the second number.");
