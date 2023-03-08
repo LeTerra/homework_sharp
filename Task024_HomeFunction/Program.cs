9d992f2fe11875bd243e7a233482c5c18ae1ecbc
LeTerra <t.v.sitdikov@gmail.com>
2023-03-08 10:06:36 +0100

    Task024 - Finished.

---
+++ Task024_HomeFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumFunction(int number)
+{
+    int sum = 0;
+    for(int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int InputNumber01 = ReadInt("Please input your number: ");
+int FinalSum = SumFunction(InputNumber01);
+Console.WriteLine($"The sum of numbers from 1 to {InputNumber01} is {FinalSum}.");
