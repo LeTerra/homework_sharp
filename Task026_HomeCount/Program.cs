cde73a8d96aa69b1f3c016c3565853ae98c940c1
LeTerra <t.v.sitdikov@gmail.com>
2023-03-08 10:48:11 +0100

    Task026 - Finished.

---
+++ Task026_HomeCount/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int CountFunction(int number)
+{
+    int countInt = 0;
+    if(number <= -1) number = number * -1;
+    for(int i = number; i >= 1; i = i/10)
+    {
+        countInt++;
+    }
+    return countInt;
+}
+
+int NumberInput = ReadInt("Please input your number: ");
+int Result = CountFunction(NumberInput);
+Console.WriteLine($"Number {NumberInput} has {Result} digits.");
