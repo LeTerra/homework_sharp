64b960c1b75967f0c402936583164e63bd70ef2d
LeTerra <t.v.sitdikov@gmail.com>
2023-03-08 13:09:01 +0100

    Task029 - Finished.

---
+++ Task029_HomeArrayInput/Program.cs
@@ -0,0 +1,15 @@
+﻿   
+int[] array = new int [8];
+int i = 0;
+Console.WriteLine("Please input the first number of the array: ");
+int VarArray = Convert.ToInt32(Console.ReadLine());
+array[i] = VarArray;
+   
+for(i = 1; i < array.Length; i++)
+   {
+    Console.WriteLine("Please input the next number of the array: ");
+    VarArray = Convert.ToInt32(Console.ReadLine());
+    array[i] = VarArray;
+   }
+
+Console.WriteLine(string.Join(", ", array));
