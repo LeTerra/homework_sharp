5105431c601071acb589da544cadb83152753fa2
LeTerra <t.v.sitdikov@gmail.com>
2023-03-08 11:18:01 +0100

    Task 027 - Finished.

---
+++ Task027_HomeSumCount/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int SumFunction(int number)
+{
+    int sum = 0;
+    for(int i = number; i > 1; i = i/10)
+    {
+        number = i % 10;
+        sum += number;
+    }
+    return sum;
+}
+
+
+int theNumber = ReadInt("Please input your number: ");
+int theSum = SumFunction(theNumber);
+Console.WriteLine($"The sum of all the digits in that number is {theSum}.");
