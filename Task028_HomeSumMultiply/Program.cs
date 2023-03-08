87993dbbc5a66342aa48bf3b5141ed9e36579b23
LeTerra <t.v.sitdikov@gmail.com>
2023-03-08 12:05:56 +0100

    Task028 - Finished.

---
+++ Task028_HomeSumMultiply/Program.cs
@@ -0,0 +1,18 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int MultiSumFunction(int number)
+{
+    int sum = 1;
+    for(int i = 1; i <= number; i++)
+    {
+        sum = sum*i;
+    }
+    return sum;
+}
+
+int NumberInput = ReadInt("Please input your number: ");
+int SumOutput = MultiSumFunction(NumberInput);
+Console.WriteLine($"The sum of all the multiplications from 1 to {NumberInput} is {SumOutput}.");
