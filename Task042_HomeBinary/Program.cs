835e1bfe1e8ae14376b305764209efb98824a070
LeTerra <t.v.sitdikov@gmail.com>
2023-03-10 15:04:21 +0100

    Task042 - Finished.

---
+++ Task042_HomeBinary/Program.cs
@@ -0,0 +1,17 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void BinaryNumber(int n)
+{
+    if(n <= 0) return;
+    BinaryNumber(n / 2);
+    Console.Write(n % 2);
+
+}
+
+int Number = ReadInt("Please input your number:");
+BinaryNumber(Number);
