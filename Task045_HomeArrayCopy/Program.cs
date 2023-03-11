7697ec2e98e1469e6fdbcd0896a01daaec17b84a
LeTerra <t.v.sitdikov@gmail.com>
2023-03-11 11:36:59 +0100

    Task045 - Finished.

---
+++ Task045_HomeArrayCopy/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] array = {1,2,3,4,5};
+int[] tmparray = new int[array.Length];
+for(int i = 0; i < array.Length; i++)
+{
+    tmparray[i] = array[i];
+}
+
+Console.WriteLine($"{string.Join(", ", tmparray)}");
