938a646030200a17576ea872d5827e61b08c97ee
LeTerra <t.v.sitdikov@gmail.com>
2023-03-11 13:36:39 +0100

    Task048 - Finished.

---
+++ Task048_HomeDArrayForm/Program.cs
@@ -0,0 +1,42 @@
+﻿(int, int) ReadInt(string message01, string message02)
+{
+    Console.WriteLine(message01);
+    int Number01 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(message02);
+    int Number02 = Convert.ToInt32(Console.ReadLine());
+
+    return (Number01,Number02);
+}
+
+int[,] FormDArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = j+i;
+
+        }
+    }
+    return array;
+}
+
+void PrintMethod(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+(int a, int b) = ReadInt("Please input the number of rows: ", "Please input the number of columns: ");
+Console.WriteLine();
+int[,] FormulaeArray = FormDArray(a,b);
+PrintMethod(FormulaeArray);
