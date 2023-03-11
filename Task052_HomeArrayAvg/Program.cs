da9a3fec46cfa552448a863a5ebb4fab052a5d5d
LeTerra <t.v.sitdikov@gmail.com>
2023-03-11 22:20:02 +0100

    Task052 - Finished.

---
+++ Task052_HomeArrayAvg/Program.cs
@@ -0,0 +1,62 @@
+﻿(int, int) ReadInt(string message01, string message02)
+{
+    Console.WriteLine(message01);
+    int Number01 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(message02);
+    int Number02 = Convert.ToInt32(Console.ReadLine());
+
+    return (Number01,Number02);
+}
+int[,] RndDArray(int rows, int columns)
+{
+    Random rand = new Random();
+    int[,] array = new int[rows,columns];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rand.Next(0,10);
+        }
+    }
+    return array;
+}
+void PrintMethod(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ArrayAverage(int[,] array)
+{
+    double tempSum = 0;
+    double tempAvg = 0;
+    double[] AVG = new double[array.GetLength(0)];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            tempSum += array[i,j];
+        }
+        tempAvg = tempSum/array.GetLength(1);
+        AVG[i] = Math.Round(tempAvg, 1);
+        tempSum = 0;
+    }
+    Console.WriteLine($"The list of all the row averages is such: {string.Join(", ", AVG)}.");
+}
+
+
+(int a, int b) = ReadInt("Please input the number of rows: ", "Please input the number of columns: ");
+Console.WriteLine();
+
+int[,] NewArray = RndDArray(a,b);
+PrintMethod(NewArray);
+
+Console.WriteLine();
+
+ArrayAverage(NewArray);
