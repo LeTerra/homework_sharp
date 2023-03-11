be633f047c0ed870a653dd7769a08ad0ba39645f
LeTerra <t.v.sitdikov@gmail.com>
2023-03-11 13:24:27 +0100

    Task047 - Finished.

---
+++ Task047_HomeDArrayAdv/Program.cs
@@ -0,0 +1,41 @@
+﻿(int, int) ReadInt(string message01, string message02)
+{
+    Console.WriteLine(message01);
+    int Number01 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(message02);
+    int Number02 = Convert.ToInt32(Console.ReadLine());
+
+    return (Number01,Number02);
+}
+
+double[,] RndDArray(int rows, int columns)
+{
+    Random rand = new Random();
+    double[,] array = new double[rows,columns];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Math.Round((rand.NextDouble()*(10+10)-10), 2);  
+        }
+    }
+    return array;
+}
+
+void PrintMethod(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+(int a, int b) = ReadInt("Please input the number of rows: ", "Please input the number of columns: ");
+Console.WriteLine();
+double[,] RandomArray = RndDArray(a,b);
+PrintMethod(RandomArray);
