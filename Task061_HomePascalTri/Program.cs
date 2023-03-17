77dc911c541360de6500767ad81bf1c9cda7eb17
LeTerra <t.v.sitdikov@gmail.com>
2023-03-17 10:27:05 +0100

    Task061 - Relinked and refinished.

--- Task061_HomePascalTri/Program.cs
+++ Task061_HomePascalTri/Program.cs
@@ -5,9 +5,22 @@
     return number;
 }
 
+int CountInt(int number)
+{
+    int count = 0;
+    for(int i = number; i >= 1; i /= 10)
+    {
+        count++;
+    }
+    return count;
+}
+
 void PascalMethodAdv(int n)
 {
     Console.Clear();
+
+    int tempPos = 0;
+    int tempCount = 0;
 
     int xa = 50, ya = 1;
     Console.SetCursorPosition(xa, ya);
@@ -16,17 +29,15 @@ void PascalMethodAdv(int n)
     int[] array = new int[1];
     array[0] = 1;
     int[] preArray = array;
+    xa = 70; ya = 3;
     Console.SetCursorPosition(xa, ya);
     Console.Write($"{0} = {string.Join(" ", array)}");
     array = Array.Empty<int>();
 
     if(n>0)
 {
-    for(int i = 1; i < n; i++)
+    for(int i = 1; i <= n; i++)
     {
-        xa--;
-        ya++;
-        Console.SetCursorPosition(xa, ya);
         Array.Resize<int>(ref array, i+1);
         array[0] = 1;
         array[i] = 1;
@@ -36,6 +47,12 @@ void PascalMethodAdv(int n)
         {
             array[j] = (preArray[j-1] + preArray[j]);
         }
+        
+        tempPos = ((i+1)/2);
+        tempCount = CountInt(array[tempPos]);
+        xa-= tempCount;
+        ya++;
+        Console.SetCursorPosition(xa, ya);
         Console.Write($"{i} = {string.Join(" ", array)}");
         preArray = array;
         array = Array.Empty<int>();
@@ -43,9 +60,6 @@ void PascalMethodAdv(int n)
     }
 }
 }
-
-
-
 
 int InputNumber = ReadInt("Please input your number: ");
 PascalMethodAdv(InputNumber);
