aa3b694897a99d36224508f7c967899912e81d89
LeTerra <t.v.sitdikov@gmail.com>
2023-03-19 19:04:01 +0100

    Task063 - Finished.

---
+++ Task063_HomeNatural/Program.cs
@@ -0,0 +1,18 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    return n;
+}
+double RecurNum(int n)
+{
+    if(n < 0) return -1;
+    if(n == 1) return 1;
+    Console.Write($"{RecurNum(n-1)}, ");
+    return n;
+}
+
+int inputNumber = ReadInt("Please input your number:");
+Console.Write("N = ");
+Console.Write(RecurNum(inputNumber));
