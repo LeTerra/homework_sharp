8c3569976668f3fb6a7c84ba68e68a8e2041a624
LeTerra <t.v.sitdikov@gmail.com>
2023-03-19 23:04:35 +0100

    Task067 - FInished.

---
+++ Task067_HomeRecDigitSum/Program.cs
@@ -0,0 +1,21 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int SumFunction(int sum)
+{
+
+    if(sum == 0) return 0;
+    else
+    {
+        sum = sum%10 + SumFunction(sum/10);
+    }
+    return sum;
+    
+}
+
+
+int theNumber = ReadInt("Please input your number: ");
+int theSum = SumFunction(theNumber);
+Console.WriteLine($"The sum of all the digits in that number is {theSum}.");
