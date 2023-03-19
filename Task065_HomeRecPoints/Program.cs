6613f5e187382b209955c922679d25420b355085
LeTerra <t.v.sitdikov@gmail.com>
2023-03-19 20:55:52 +0100

    Task065 - Finished.

---
+++ Task065_HomeRecPoints/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    return n;
+}
+int RecurNumSelect(int nMin, int nMax)
+{
+    if(nMin == nMax) return nMin;
+    if(nMin < nMax)
+    {
+    Console.Write($"{nMin}, ");
+    RecurNumSelect(nMin+1, nMax);
+    }
+    return nMax;
+}
+
+int inputMin = ReadInt("Please input the minimum number:");
+int inputMax = ReadInt("Please input the maximum number:");
+Console.Write($"The numbers between (and incl.) {inputMin} and {inputMax} are such: ");
+Console.Write(RecurNumSelect(inputMin,inputMax));
