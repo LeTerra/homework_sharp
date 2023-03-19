e3f4cdca7ab903b425d81b01c3da2fe577d826d5
LeTerra <t.v.sitdikov@gmail.com>
2023-03-19 20:44:56 +0100

    Task064 - Finished.

---
+++ Task064_HomeRecurReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    return n;
+}
+int AdvRecurNum(int n)
+{
+    if(n <= 0) return 0;
+    if(n == 1) return 1;
+    if(n > 1)
+    {
+    Console.Write($"{n}, ");
+    AdvRecurNum(n-1);
+    }
+    return 1;
+}
+
+int inputNumber = ReadInt("Please input your number:");
+Console.Write($"N = {inputNumber}: ");
+Console.Write(AdvRecurNum(inputNumber));
