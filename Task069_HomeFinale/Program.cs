f5e111bd19203bf601d4997f13449b1a489fca2d
LeTerra <t.v.sitdikov@gmail.com>
2023-03-20 01:24:49 +0100

    Task069 - Finished, and along with it, the first course.

---
+++ Task069_HomeFinale/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int PowerFunction(int number_01, int number_02)
+{
+    if(number_02 == 0 || number_01 == 1) return 1;
+    if(number_02 > 1) number_01 *= PowerFunction(number_01, number_02-1);
+    return number_01;
+}
+
+int NumberA = ReadInt("Please input your initial number: ");
+int NumberB = ReadInt("Please input the power you want to put the initial number in: ");
+int PowerNumber = PowerFunction(NumberA,NumberB);
+Console.WriteLine($"The number for {NumberA} in power of {NumberB} would be {PowerNumber}.");
